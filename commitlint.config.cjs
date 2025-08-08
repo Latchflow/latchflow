6a974b51165def0f4ecea704cb26d99d075a8e59
David White <whited9339@gmail.com>
2025-08-08 20:37:47 +0000

    plugin: pdated commitlint type enum to include some project specific types

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,25 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",        // new feature
+        "fix",         // bug fix
+        "docs",        // documentation changes
+        "style",       // formatting, no code change
+        "refactor",    // code restructure without behavior change
+        "perf",        // performance improvements
+        "test",        // adding/updating tests
+        "build",       // build system or external dependencies
+        "ci",          // CI/CD changes
+        "chore",       // maintenance, package updates
+        "revert",      // reverting changes
+        "plugin",      // new/updated Latchflow plugin
+        "schema",      // DB schema changes
+        "security"     // security-related fixes
+      ]
+    ]
+  }
+};
